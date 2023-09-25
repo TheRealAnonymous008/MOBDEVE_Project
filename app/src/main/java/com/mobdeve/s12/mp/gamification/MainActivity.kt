5b4d990c537b0b6263edbd28cd2c8ab25f7f910e
Joaquin dela Rosa <62501229+TheRealAnonymous008@users.noreply.github.com>
2023-09-25 08:16:08 +0800

    Initial commit

---
+++ app/src/main/java/com/mobdeve/s12/mp/gamification/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s12.mp.gamification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
