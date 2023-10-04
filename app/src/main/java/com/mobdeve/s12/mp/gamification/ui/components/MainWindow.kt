f9aff1de3f7e5bd890776f1627cf8d62281db90b
Sakui <63701743+Sakui-ph@users.noreply.github.com>
2023-10-04 23:17:47 +0800

    update: make main screen look a little more bearable

--- app/src/main/java/com/mobdeve/s12/mp/gamification/ui/components/MainWindow.kt
+++ app/src/main/java/com/mobdeve/s12/mp/gamification/ui/components/MainWindow.kt
@@ -1,6 +1,7 @@
 package com.mobdeve.s12.mp.gamification.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,18 +9,26 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.mobdeve.s12.mp.gamification.model.Profile
 import com.mobdeve.s12.mp.gamification.model.Skill
 import com.mobdeve.s12.mp.gamification.model.Task
+import com.mobdeve.s12.mp.gamification.ui.theme.Background
 import com.mobdeve.s12.mp.gamification.ui.theme.MOBDEVEProjectTheme
+import com.mobdeve.s12.mp.gamification.ui.theme.PrimaryColor
 import com.mobdeve.s12.mp.gamification.ui.theme.SecondaryColor
 
 
@@ -30,23 +39,26 @@ fun MainWindow(profile : Profile, list : ArrayList<Task>, skills : ArrayList<Ski
     MOBDEVEProjectTheme{
         // A surface container using the 'background' color from the theme
         Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
+            color = Background
         ) {
             Column(
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp)
             ) {
                 ProfileHeader(profile)
 
                 Spacer(modifier = Modifier.height(10.dp))
 
-                Box(
+                ElevatedCard (
+                    colors = CardDefaults.cardColors(
+                        containerColor = SecondaryColor
+                    ),
+                    shape = RoundedCornerShape(25.dp),
                     modifier = Modifier
-                        .background(SecondaryColor)
-                        .fillMaxHeight()
-                        .fillMaxWidth()
-                ){
+                        .height(550.dp)
 
+                ){
                     TaskList(taskList = list)
 //                    SkillList(skills)
                 }
