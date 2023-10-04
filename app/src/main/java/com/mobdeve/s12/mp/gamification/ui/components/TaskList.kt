dd49fe4aecb79e6fa3c9586b67602cdf4cc60fd6
Joaquin dela Rosa <62501229+TheRealAnonymous008@users.noreply.github.com>
2023-10-04 15:28:20 +0800

    feat: buttons in task view

--- app/src/main/java/com/mobdeve/s12/mp/gamification/ui/components/TaskList.kt
+++ app/src/main/java/com/mobdeve/s12/mp/gamification/ui/components/TaskList.kt
@@ -3,21 +3,20 @@ package com.mobdeve.s12.mp.gamification.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.unit.dp
 import com.mobdeve.s12.mp.gamification.model.Task
 import com.mobdeve.s12.mp.gamification.ui.theme.PrimaryColor
-import com.mobdeve.s12.mp.gamification.ui.theme.SecondaryColor
 import com.mobdeve.s12.mp.gamification.ui.theme.TextColor
 
 @Composable
@@ -40,24 +39,48 @@ fun TaskEntry(task : Task){
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(bottom=10.dp)
+            .padding(bottom = 10.dp)
     ){
-        Column(
+        Box(
             modifier = Modifier
                 .background(PrimaryColor)
                 .fillMaxWidth()
                 .padding(20.dp)
                 .padding(start = 40.dp)
         ) {
-            Text(
-                text = task.title,
-                color = TextColor
-            )
+            Row(
 
-            Text(
-                text = task.description,
-                color = TextColor
-            )
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    // Task title
+                    Text(
+                        text = task.title,
+                        color = TextColor
+                    )
+
+                    // Task description
+                    Text(
+                        text = task.description,
+                        color = TextColor
+                    )
+                }
+
+                Column (
+                    modifier = Modifier.
+                        fillMaxWidth(),
+                    horizontalAlignment = Alignment.End
+                ){
+                    Button(
+                        onClick = { /*  TODO: Add button click logic */ }
+                    ) {
+                        Text(
+                            text = "Play"
+                        )
+                    }
+                }
+            }
         }
     }
 }
