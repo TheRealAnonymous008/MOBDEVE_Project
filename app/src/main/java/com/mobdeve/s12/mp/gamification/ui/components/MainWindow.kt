c6f15eb04becf685cc3531a9d0b63f75618d8967
Sakui <63701743+Sakui-ph@users.noreply.github.com>
2023-10-05 11:08:48 +0800

    Revert "update: make main screen look a little more bearable"
    This reverts commit f9aff1de3f7e5bd890776f1627cf8d62281db90b.

--- app/src/main/java/com/mobdeve/s12/mp/gamification/ui/components/MainWindow.kt
+++ app/src/main/java/com/mobdeve/s12/mp/gamification/ui/components/MainWindow.kt
@@ -1,7 +1,6 @@
 package com.mobdeve.s12.mp.gamification.ui.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,26 +8,18 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.mobdeve.s12.mp.gamification.model.Profile
 import com.mobdeve.s12.mp.gamification.model.Skill
 import com.mobdeve.s12.mp.gamification.model.Task
-import com.mobdeve.s12.mp.gamification.ui.theme.Background
 import com.mobdeve.s12.mp.gamification.ui.theme.MOBDEVEProjectTheme
-import com.mobdeve.s12.mp.gamification.ui.theme.PrimaryColor
 import com.mobdeve.s12.mp.gamification.ui.theme.SecondaryColor
 
 
@@ -39,26 +30,23 @@ fun MainWindow(profile : Profile, list : ArrayList<Task>, skills : ArrayList<Ski
     MOBDEVEProjectTheme{
         // A surface container using the 'background' color from the theme
         Surface(
-            color = Background
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
         ) {
             Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(10.dp)
+                modifier = Modifier.fillMaxSize()
             ) {
                 ProfileHeader(profile)
 
                 Spacer(modifier = Modifier.height(10.dp))
 
-                ElevatedCard (
-                    colors = CardDefaults.cardColors(
-                        containerColor = SecondaryColor
-                    ),
-                    shape = RoundedCornerShape(25.dp),
+                Box(
                     modifier = Modifier
-                        .height(550.dp)
-
+                        .background(SecondaryColor)
+                        .fillMaxHeight()
+                        .fillMaxWidth()
                 ){
+
                     TaskList(taskList = list)
 //                    SkillList(skills)
                 }
--- app/src/main/java/com/mobdeve/s12/mp/gamification/ui/components/TaskEntry.kt
+++ app/src/main/java/com/mobdeve/s12/mp/gamification/ui/components/TaskEntry.kt
@@ -1,7 +1,6 @@
 package com.mobdeve.s12.mp.gamification.ui.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -10,23 +9,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.DefaultShadowColor
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.mobdeve.s12.mp.gamification.model.Task
 import com.mobdeve.s12.mp.gamification.ui.theme.PrimaryColor
@@ -35,22 +23,10 @@ import com.mobdeve.s12.mp.gamification.ui.theme.TextColor
 
 @Composable
 fun TaskEntry(task : Task){
-    Card (
-        shape = (
-            RoundedCornerShape(size = 45.dp)
-        ),
+    Box(
         modifier = Modifier
-            .height(135.dp)
-            .padding(
-                bottom = 15.dp,
-            )
-            .shadow(
-                elevation = 10.dp,
-                shape = RoundedCornerShape(size = 30.dp),
-                ambientColor = DefaultShadowColor,
-                spotColor = DefaultShadowColor
-            )
-
+            .fillMaxWidth()
+            .padding(bottom = 10.dp)
     ){
         Box(
             modifier = Modifier
