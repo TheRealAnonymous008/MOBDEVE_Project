47ef6a2cdbde9bb30c838d2dd2612820252be542
Joaquin dela Rosa <62501229+TheRealAnonymous008@users.noreply.github.com>
2023-10-06 19:01:35 +0800

    feat: add dismissable tasks

--- app/src/main/java/com/mobdeve/s12/mp/gamification/ui/components/TaskEntry.kt
+++ app/src/main/java/com/mobdeve/s12/mp/gamification/ui/components/TaskEntry.kt
@@ -1,5 +1,6 @@
 package com.mobdeve.s12.mp.gamification.ui.components
 
+import android.util.Log
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.SpringSpec
 import androidx.compose.animation.core.animateFloatAsState
@@ -19,10 +20,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.DismissState
+import androidx.compose.material3.DismissValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -39,24 +43,28 @@ import com.mobdeve.s12.mp.gamification.ui.theme.TextColor
 import kotlin.math.roundToInt
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskEntry(task : Task){
-    var offset by remember { mutableStateOf(0f) }
-    var isDragging by remember { mutableStateOf(false)}
-    var isReleased by remember { mutableStateOf(false) }
-
-    val springSpec = spring<Float>(stiffness = Spring.StiffnessLow)
-
-    // Animation parameters
-    val targetOffset = if (isDragging) offset else 0f
-
-    // Animate the offset using a spring animation when isDragging changes
-    val animatedOffset by animateFloatAsState(
-        targetValue = targetOffset,
-        animationSpec = if (isReleased) springSpec else SpringSpec(1f, stiffness = Spring.StiffnessVeryLow),
-        label = ""
+    var show by remember {mutableStateOf(true) }
+    val dismissState = rememberDismissState(
+        confirmValueChange = {
+            if (it == DismissValue.DismissedToStart || it == DismissValue.DismissedToEnd) {
+                show = false
+                true
+            } else false
+        }, positionalThreshold = { 150.dp.toPx() }
     )
 
+
+    SwipeToDismiss(
+        state = dismissState,
+        background = {},
+        dismissContent = {TaskEntryBox(task)})
+}
+
+@Composable 
+fun TaskEntryBox(task: Task){
     Card (
         modifier = Modifier
             .fillMaxWidth()
@@ -67,26 +75,6 @@ fun TaskEntry(task : Task){
             modifier = Modifier
                 .fillMaxSize()
                 .background(PrimaryColor)
-                .pointerInput(Unit) {
-                    detectTransformGestures { _, pan, _, _ ->
-                        val horizontalOffset = pan.x
-
-                        // Only allow dragging from the left (horizontalOffset should be positive)
-                        if (horizontalOffset >= 0) {
-                            offset += horizontalOffset
-
-                            // Restrict the offset within a range (0 to 300)
-                            if (offset < 0) {
-                                offset = 0f
-                            } else if (offset > 300) {
-                                offset = 300f
-                            }
-
-                            isDragging = true
-                        }
-                    }
-                }
-                .offset { IntOffset(x = animatedOffset.roundToInt(), y = 0) }
         ) {
             Row {
                 Spacer(Modifier.width(10.dp))
@@ -143,22 +131,6 @@ fun TaskEntry(task : Task){
                     }
                 }
             }
-        }
-    }
-
-    DisposableEffect(isReleased) {
-        if (!isDragging && isReleased) {
-            isReleased = false
-            isDragging = false
-            offset = 0f
-        }
-
-        onDispose { }
-    }
-
-    LaunchedEffect(isDragging) {
-        if (!isDragging) {
-            isReleased = true
         }
     }
 }
