5fa35f593514facf634b616ac5a3b3e5a207f397
Sakui <63701743+Sakui-ph@users.noreply.github.com>
2023-11-13 11:57:27 +0800

    update: increase default cosmetics

--- app/src/main/java/com/mobdeve/s12/mp/gamification/model/Cosmetic.kt
+++ app/src/main/java/com/mobdeve/s12/mp/gamification/model/Cosmetic.kt
@@ -60,6 +60,35 @@ fun createDefaultCosmeticList() : ArrayList<Cosmetic> {
         image = R.drawable.cosmetic_default_feet,
         description = "The default feet"
     ))
+    cosmeticList.add(HeadCosmetic(
+        id = 0,
+        name = "Stark's Hair",
+        cost = 0,
+        image = R.drawable.cosmetic_stark_head,
+        description = "Short red hair with a dark contrasting black in the center."
+    ))
+    cosmeticList.add(TorsoCosmetic(
+        id = 0,
+        name = "Stark's Coat",
+        cost = 0,
+        image = R.drawable.cosmetic_stark_torso,
+        description = "Stark's coat"
+    ))
+    cosmeticList.add(LegsCosmetic(
+        id = 0,
+        name = "Stark's Pants",
+        cost = 0,
+        image = R.drawable.cosmetic_stark_legs,
+        description = "Stark's pants"
+    ))
+    cosmeticList.add(FeetCosmetic(
+        id = 0,
+        name = "Stark Shoes",
+        cost = 0,
+        image = R.drawable.cosmetic_stark_feet,
+        description = "Stark's shoes"
+    ))
+
 
     return cosmeticList
 }
