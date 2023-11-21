3732da8402a27e813caad79693f8b9b1ff72bd3f
migsruelos <87805608+migsruelos@users.noreply.github.com>
2023-11-21 20:16:25 +0800

    Update TaskDetails.kt

--- app/src/main/java/com/mobdeve/s12/mp/gamification/ui/components/tasks/TaskDetails.kt
+++ app/src/main/java/com/mobdeve/s12/mp/gamification/ui/components/tasks/TaskDetails.kt
@@ -1,54 +1,35 @@
 package com.mobdeve.s12.mp.gamification.ui.components.tasks
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Dialog
-import com.mobdeve.s12.mp.gamification.R
 import com.mobdeve.s12.mp.gamification.localdb.AppDatabase
 import com.mobdeve.s12.mp.gamification.localdb.RepositoryHolder
 import com.mobdeve.s12.mp.gamification.model.Profile
 import com.mobdeve.s12.mp.gamification.model.Task
 import com.mobdeve.s12.mp.gamification.model.TimeInfo
 import com.mobdeve.s12.mp.gamification.model.createDefaultTask
+import com.mobdeve.s12.mp.gamification.model.formatTimestampDate
 import com.mobdeve.s12.mp.gamification.model.generateDefaultProfile
 import com.mobdeve.s12.mp.gamification.ui.theme.OtherAccent
 import com.mobdeve.s12.mp.gamification.ui.theme.SecondaryColor
 import com.mobdeve.s12.mp.gamification.ui.theme.TextColor
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Date
-import java.util.Locale
-import com.mobdeve.s12.mp.gamification.ui.theme.body1
+import java.sql.Timestamp
 
 @Composable
-fun TaskDetailsLayout(
-    task : Task,
-    profile : Profile,
-    onDelete : () -> Unit,
-    repo: RepositoryHolder) {
-
+fun TaskDetailsLayout(task : Task, profile : Profile, onDelete : () -> Unit, repo: RepositoryHolder) {
     var title by remember { mutableStateOf(task.title)}
     var description by remember { mutableStateOf(task.description) }
+    var timeInfo by remember { mutableStateOf(task.timeInfo) }
 
     Column(
         modifier = Modifier
@@ -140,20 +121,16 @@ fun TaskDetailsLayout(
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        TimeInfoDetails(timeInfo = task.timeInfo)
+        // Update TimeInfo
+        TimeInfoDetails(
+            timeInfo = timeInfo,
+            onUpdate = { updatedTimeInfo: TimeInfo ->
+                timeInfo = updatedTimeInfo
+                task.timeInfo = updatedTimeInfo
+            }
+        )
 
-        // Edit TimeInfo Button
-        Button(
-            onClick = {
-                // Set showDialog to true to trigger the dialog
-                showDialog = true
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-        ) {
-            Text("Edit Time Info", fontWeight = FontWeight.Bold)
-        }
+        Spacer(modifier = Modifier.height(16.dp))
 
         // Rewards list
         Text(
@@ -182,226 +159,101 @@ fun TaskDetailsLayout(
         ) {
             Text("Delete Task", fontWeight = FontWeight.Bold)
         }
-
-        // Use Dialog to display the TimeInfoEditDialog when showDialog is true
-        if (showDialog) {
-            AlertDialog(
-                onDismissRequest = {
-                    showDialog = false
-                },
-                title = {
-                    Text("Edit Time Info")
-                },
-                text = {
-                    TimeInfoEditDialog(
-                        initialTimeInfo = task.timeInfo,
-                        onTimeInfoUpdated = { updatedTimeInfo ->
-                            task.timeInfo = updatedTimeInfo
-                            repo.taskRepository.update(task)
-                        },
-                        onDismiss = {
-                            showDialog = false
-                        }
-                    )
-                },
-                confirmButton = {
-                    // You can add confirm button if needed
-                },
-                dismissButton = {
-                    // You can add dismiss button if needed
-                }
-            )
-        }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TimeInfoEditDialog(
-    initialTimeInfo: TimeInfo,
-    onTimeInfoUpdated: (TimeInfo) -> Unit,
-    onDismiss: () -> Unit
-) {
-    var updatedTimeInfo by remember { mutableStateOf(initialTimeInfo) }
-    var showTimePicker by remember { mutableStateOf(false) }
-
-    AlertDialog(
-        onDismissRequest = { onDismiss() },
-        title = {
-            Text("Edit Time Info")
-        },
-        text = {
-            // Use the CustomTimePicker composable when the user clicks on "Edit Time"
-            if (showTimePicker) {
-                CustomTimePicker(
-                    selectedTime = updatedTimeInfo.datetimeFrom ?: Date(),
-                    onTimeSelected = {
-                        updatedTimeInfo = updatedTimeInfo.copy(datetimeFrom = it)
-                        showTimePicker = false
-                    }
-                )
-            } else {
-                // Display selected time
-                Text(
-                    text = SimpleDateFormat("hh:mm a", Locale.getDefault()).format(updatedTimeInfo.datetimeFrom ?: Date()),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { showTimePicker = true }
-                        .padding(vertical = 8.dp, horizontal = 16.dp),
-                    style = body1
-                )
-            }
-        },
-        confirmButton = {
-            Button(
-                onClick = {
-                    // Call the callback with the updated TimeInfo
-                    onTimeInfoUpdated(updatedTimeInfo)
-                    onDismiss()
-                }
-            ) {
-                Text("Confirm")
-            }
-        },
-        dismissButton = {
-            Button(
-                onClick = {
-                    onDismiss()
-                }
-            ) {
-                Text("Cancel")
-            }
-        }
-    )
-}
-
-
-
-@OptIn(ExperimentalComposeUiApi::class)
-@Composable
-fun CustomTimePicker(
-    selectedTime: Date,
-    onTimeSelected: (Date) -> Unit
-) {
-    var time by remember { mutableStateOf(selectedTime) }
-    var showTimePicker by remember { mutableStateOf(false) }
-
+fun TimeInfoDetails(timeInfo: TimeInfo, onUpdate: (TimeInfo) -> Unit) {
     Column(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
     ) {
-        // Display selected time
+        // Date time created (read-only)
         Text(
-            text = SimpleDateFormat("hh:mm a", Locale.getDefault()).format(time),
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable { showTimePicker = true }
-                .padding(vertical = 8.dp, horizontal = 16.dp),
-            style = MaterialTheme.typography.body1
+            text = "Created: ${formatTimestampDate(timeInfo.datetimeCreated)}",
+            fontSize = 14.sp,
+            color = TextColor,
         )
 
-        // Custom time picker dialog
-        if (showTimePicker) {
-            CustomTimePickerDialog(
-                initialTime = time,
-                onTimeSelected = {
-                    time = it
-                    onTimeSelected(it)
-                    showTimePicker = false
-                },
-                onDismiss = { showTimePicker = false }
-            )
-        }
-    }
-}
+        Spacer(modifier = Modifier.height(8.dp))
 
-@OptIn(ExperimentalComposeUiApi::class)
-@Composable
-fun CustomTimePickerDialog(
-    initialTime: Date,
-    onTimeSelected: (Date) -> Unit,
-    onDismiss: () -> Unit
-) {
-    var hour by remember { mutableStateOf(0) }
-    var minute by remember { mutableStateOf(0) }
-    val context = LocalContext.current
-    val keyboardController = LocalSoftwareKeyboardController.current
-
-    Dialog(
-        onDismissRequest = {
-            keyboardController?.hide()
-            onDismiss()
-        }
-    ) {
-        Column(
+        // Date time from (editable)
+        BasicTextField(
+            value = timeInfo.datetimeFrom?.toString() ?: "",
+            onValueChange = {
+                // Handle updating datetimeFrom
+                // You might want to add proper validation and conversion here
+                timeInfo.datetimeFrom = Timestamp.valueOf(it)
+                onUpdate(timeInfo)
+            },
+            textStyle = TextStyle(
+                fontSize = 14.sp,
+                color = TextColor,
+            ),
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                // Hour input
-                OutlinedTextField(
-                    value = hour.toString(),
-                    onValueChange = {
-                        if (it.isNotBlank()) {
-                            hour = it.toInt()
-                        }
-                    },
-                    label = { Text("Hour") },
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Number
-                    ),
-                    keyboardActions = KeyboardActions(
-                        onDone = {
-                            keyboardController?.hide()
-                        }
-                    ),
+                .background(Color.Transparent)
+                .fillMaxWidth(),
+            decorationBox = { innerTextField ->
+                Box(
                     modifier = Modifier
-                        .weight(1f)
-                        .padding(end = 8.dp)
-                )
-
-                // Minute input
-                OutlinedTextField(
-                    value = minute.toString(),
-                    onValueChange = {
-                        if (it.isNotBlank()) {
-                            minute = it.toInt()
-                        }
-                    },
-                    label = { Text("Minute") },
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Number
-                    ),
-                    keyboardActions = KeyboardActions(
-                        onDone = {
-                            keyboardController?.hide()
-                        }
-                    ),
-                    modifier = Modifier.weight(1f)
-                )
+                        .background(Color.Transparent)
+                        .fillMaxWidth()
+                ) {
+                    if (timeInfo.datetimeFrom == null) {
+                        Text(
+                            text = "From: Not set",
+                            fontSize = 14.sp,
+                            color = Color.LightGray
+                        )
+                    }
+                    innerTextField()
+                }
             }
+        )
 
-            Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(8.dp))
 
-            // Confirm button
-            Button(
-                onClick = {
-                    val selectedTime = Calendar.getInstance()
-                    selectedTime.set(Calendar.HOUR_OF_DAY, hour)
-                    selectedTime.set(Calendar.MINUTE, minute)
-                    onTimeSelected(selectedTime.time)
-                    onDismiss()
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-            ) {
-                Text("Confirm")
+        // Date time to (editable)
+        BasicTextField(
+            value = timeInfo.dateTimeTo?.toString() ?: "",
+            onValueChange = {
+                // Handle updating dateTimeTo
+                // You might want to add proper validation and conversion here
+                timeInfo.dateTimeTo = Timestamp.valueOf(it)
+                onUpdate(timeInfo)
+            },
+            textStyle = TextStyle(
+                fontSize = 14.sp,
+                color = TextColor,
+            ),
+            modifier = Modifier
+                .background(Color.Transparent)
+                .fillMaxWidth(),
+            decorationBox = { innerTextField ->
+                Box(
+                    modifier = Modifier
+                        .background(Color.Transparent)
+                        .fillMaxWidth()
+                ) {
+                    if (timeInfo.dateTimeTo == null) {
+                        Text(
+                            text = "To: Not set",
+                            fontSize = 14.sp,
+                            color = Color.LightGray
+                        )
+                    }
+                    innerTextField()
+                }
             }
-        }
+        )
+
+        // Add other properties as needed
+
+        // Progress
+        Text(
+            text = "Progress: ${timeInfo.progress}%",
+            fontSize = 14.sp,
+            color = TextColor,
+        )
     }
 }
-
-
