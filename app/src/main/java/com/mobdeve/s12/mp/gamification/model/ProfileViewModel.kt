b39958ba61fd8ebce6fadd6e8f584162995b86bd
Miguel Ruelos <87805608+migsruelos@users.noreply.github.com>
2023-11-22 04:48:31 +0800

    Create ProfileViewModel.kt

---
+++ app/src/main/java/com/mobdeve/s12/mp/gamification/model/ProfileViewModel.kt
@@ -0,0 +1,63 @@
+package com.mobdeve.s12.mp.gamification.model
+
+// ProfileViewModel.kt
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mobdeve.s12.mp.gamification.model.ProfileDetails
+import kotlinx.coroutines.launch
+
+class ProfileViewModel(private val context: Context) : ViewModel() {
+
+    private val sharedPreferences: SharedPreferences =
+        context.getSharedPreferences("profile_prefs", Context.MODE_PRIVATE)
+
+    private val _profileDetails = mutableStateOf(getProfileDetails())
+    val profileDetails: State<ProfileDetails> get() = _profileDetails
+
+    fun updateName(newName: String) {
+        val updatedProfile = _profileDetails.value.copy(name = newName)
+        saveProfileDetails(updatedProfile)
+    }
+
+    fun updateDescription(newDescription: String) {
+        val updatedProfile = _profileDetails.value.copy(description = newDescription)
+        saveProfileDetails(updatedProfile)
+    }
+
+    fun updateAvatar(newAvatar: Avatar) {
+        val updatedProfile = _profileDetails.value.copy(avatar = newAvatar)
+        saveProfileDetails(updatedProfile)
+    }
+
+    fun updateCurrency(newCurrency: Int) {
+        val updatedProfile = _profileDetails.value.copy(currency = newCurrency)
+        saveProfileDetails(updatedProfile)
+    }
+
+    private fun saveProfileDetails(profileDetails: ProfileDetails) {
+        viewModelScope.launch {
+            with(sharedPreferences.edit()) {
+                putString("name", profileDetails.name)
+                putString("description", profileDetails.description)
+                putString("avatar", profileDetails.avatar.toJson())
+                apply()
+            }
+            _profileDetails.value = profileDetails
+        }
+    }
+
+    private fun getProfileDetails(): ProfileDetails {
+        val name = sharedPreferences.getString("name", "") ?: ""
+        val description = sharedPreferences.getString("description", "") ?: ""
+        val avatarJson = sharedPreferences.getString("avatar", "") ?: ""
+        val avatar = Avatar.fromJson(avatarJson)
+        val currency = sharedPreferences.getInt("currency", 0)
+
+        return ProfileDetails(name = name, description = description, avatar = avatar, currency = currency)
+    }
+}
