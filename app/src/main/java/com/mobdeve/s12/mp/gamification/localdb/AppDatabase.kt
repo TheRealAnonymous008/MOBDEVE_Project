00346363f11f700a2428acf506f8821700be229d
Sakui <63701743+Sakui-ph@users.noreply.github.com>
2023-11-21 19:42:22 +0800

    refactor: cosmetics

--- app/src/main/java/com/mobdeve/s12/mp/gamification/localdb/AppDatabase.kt
+++ app/src/main/java/com/mobdeve/s12/mp/gamification/localdb/AppDatabase.kt
@@ -14,6 +14,7 @@ import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.mobdeve.s12.mp.gamification.BuildConfig
 import com.mobdeve.s12.mp.gamification.R
+import com.mobdeve.s12.mp.gamification.helpers.initializeCosmetics
 import com.mobdeve.s12.mp.gamification.model.Cosmetic
 import com.mobdeve.s12.mp.gamification.model.CosmeticTypes
 import com.mobdeve.s12.mp.gamification.model.FeetCosmetic
@@ -105,82 +106,7 @@ abstract class AppDatabase : RoomDatabase() {
 
         suspend fun populateCosmetics(dao: CosmeticDao, context: Context) {
             dao.deleteAll()
-            val cosmeticList : ArrayList<Cosmetic> = ArrayList<Cosmetic>()
-
-            cosmeticList.add(
-                HeadCosmetic(
-                    id = 0,
-                    name = "Default Head",
-                    cost = 0,
-                    image = context.resources.getResourceName(R.drawable.cosmetic_default_head),
-                    description = "The default head"
-                )
-            )
-            cosmeticList.add(
-                TorsoCosmetic(
-                    id = 0,
-                    name = "Default Torso",
-                    cost = 0,
-                    image = context.resources.getResourceName(R.drawable.cosmetic_default_torso),
-                    description = "The default torso"
-                )
-            )
-            cosmeticList.add(
-                LegsCosmetic(
-                    id = 0,
-                    name = "Default Legs",
-                    cost = 0,
-                    image = context.resources.getResourceName(R.drawable.cosmetic_default_legs),
-                    description = "The default legs"
-                )
-            )
-            cosmeticList.add(
-                FeetCosmetic(
-                    id = 0,
-                    name = "Default Feet",
-                    cost = 0,
-                    image = context.resources.getResourceName(R.drawable.cosmetic_default_feet),
-                    description = "The default feet"
-                )
-            )
-            cosmeticList.add(
-                HeadCosmetic(
-                    id = 0,
-                    name = "Stark's Hair",
-                    cost = 0,
-                    image = context.resources.getResourceName(R.drawable.cosmetic_stark_head),
-                    description = "Short red hair with a dark contrasting black in the center."
-                )
-            )
-            cosmeticList.add(
-                TorsoCosmetic(
-                    id = 0,
-                    name = "Stark's Coat",
-                    cost = 0,
-                    image = context.resources.getResourceName(R.drawable.cosmetic_stark_torso),
-                    description = "Stark's coat"
-                )
-            )
-            cosmeticList.add(
-                LegsCosmetic(
-                    id = 0,
-                    name = "Stark's Pants",
-                    cost = 0,
-                    image = context.resources.getResourceName(R.drawable.cosmetic_stark_legs),
-                    description = "Stark's pants"
-                )
-            )
-            cosmeticList.add(
-                FeetCosmetic(
-                    id = 0,
-                    name = "Stark's Shoes",
-                    cost = 0,
-                    image = context.resources.getResourceName(R.drawable.cosmetic_stark_feet),
-                    description = "Stark's shoes"
-                )
-            )
-
-
+            val cosmeticList : ArrayList<Cosmetic> = initializeCosmetics(context)
 
             cosmeticList.forEach {
                 val t = getCosmeticEntity(it)
