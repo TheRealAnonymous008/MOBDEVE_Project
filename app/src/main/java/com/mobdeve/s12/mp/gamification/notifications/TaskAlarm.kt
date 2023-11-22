fcc0225b3dc0243a12f41ab2797a685c322bcd3a
TheRealAnonymous008 <joaquinantoniodelarosa@gmail.com>
2023-11-22 21:54:48 +0800

    fix: bug with alarm

--- app/src/main/java/com/mobdeve/s12/mp/gamification/notifications/TaskAlarm.kt
+++ app/src/main/java/com/mobdeve/s12/mp/gamification/notifications/TaskAlarm.kt
@@ -13,28 +13,28 @@ import java.util.Calendar
 
 @RequiresApi(Build.VERSION_CODES.S)
 fun notifyTask(context: Context, task : Task){
-    val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-    val intent = Intent(context, AlarmReceiver::class.java)
-    val pendingIntent = PendingIntent.getBroadcast(context, task.id.toInt(), intent, PendingIntent.FLAG_IMMUTABLE)
-    alarmManager.cancel(pendingIntent)
+//    val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+//    val intent = Intent(context, AlarmReceiver::class.java)
+//    val pendingIntent = PendingIntent.getBroadcast(context, task.id.toInt(), intent, PendingIntent.FLAG_IMMUTABLE)
+//    alarmManager.cancel(pendingIntent)
+//
 
-
-    val scheduledTime = task.timeInfo.datetimeFrom ?: return
-
-    val calendar: Calendar = Calendar.getInstance().apply {
-        timeInMillis = scheduledTime.time
-    }
-
-    if (alarmManager.canScheduleExactAlarms()) {
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
-    } else {
-        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
-    }
+//    val scheduledTime = task.timeInfo.datetimeFrom ?: return
+//
+//    val calendar: Calendar = Calendar.getInstance().apply {
+//        timeInMillis = scheduledTime.time
+//    }
+//
+//    if (alarmManager.canScheduleExactAlarms()) {
+//        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+//    } else {
+//        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+//    }
 }
 
 fun unnotifyTask(context: Context, task : Task){
-    val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-    val intent = Intent(context, AlarmReceiver::class.java)
-    val pendingIntent = PendingIntent.getBroadcast(context, task.id.toInt(), intent, PendingIntent.FLAG_IMMUTABLE)
-    alarmManager.cancel(pendingIntent)
+//    val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+//    val intent = Intent(context, AlarmReceiver::class.java)
+//    val pendingIntent = PendingIntent.getBroadcast(context, task.id.toInt(), intent, PendingIntent.FLAG_IMMUTABLE)
+//    alarmManager.cancel(pendingIntent)
 }
